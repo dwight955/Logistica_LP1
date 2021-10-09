b7c65ef45224705acf725affe69e46a83bfeeea5
dwight955 <dwightht.anthony955@gmail.com>
2021-10-08 19:55:42 -0500

    Comienzo del proyecto

---
+++ src/com/modulo/gui/frmPrincipal.java
@@ -0,0 +1,81 @@
+package com.modulo.gui;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JToolBar;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+public class frmPrincipal extends JFrame implements ActionListener {
+	
+	
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmPrincipal frame = new frmPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public frmPrincipal() {
+		setTitle("LOGISTICA");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 570, 425);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnSistema = new JMenu("Sistema");
+		menuBar.add(mnSistema);
+		
+		JMenuItem mntmCerrar = new JMenuItem("Cerrar");
+		mnSistema.add(mntmCerrar);
+		
+		JMenu mnMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(mnMantenimiento);
+		
+		JMenu mnReporte = new JMenu("Reporte");
+		menuBar.add(mnReporte);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+	public void actionPerformed(ActionEvent e) {
+	}
+	void mensaje(String m) {
+		JOptionPane.showMessageDialog(this, m);
+	}
+}
---
+++ src/com/modulo/utils/MySqlConexion.java
@@ -0,0 +1,25 @@
+package com.modulo.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConexion {
+	
+	public static Connection getConexion() {
+		Connection cn=null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url,user,pass;
+			url="jdbc:mysql://localhost/modelo_CL1_LPI_2021_21?serverTimezone=UTC";
+			user="root";
+			pass="mysql";
+			cn=DriverManager.getConnection(url,user,pass);	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return cn;
+	}
+	
+}
