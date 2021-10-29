cb9863be721bef3e733b5a12924e3908e85ae14c
GaryFrZero <garyesp@hotmail.com>
2021-10-28 19:04:52 -0500

    Formularios

--- src/com/logistica/gui/frmPrincipal.java
+++ src/com/logistica/gui/frmPrincipal.java
@@ -14,8 +14,15 @@ import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
+
+import hora.MostraHora;
+
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.awt.event.ActionEvent;
 import javax.swing.JToolBar;
 import javax.swing.JMenuBar;
@@ -24,6 +31,8 @@ import javax.swing.JMenu;
 import javax.swing.BoxLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Frame;
+import java.awt.SystemColor;
 
 @SuppressWarnings("serial")
 public class frmPrincipal extends JFrame implements ActionListener {
@@ -31,8 +40,11 @@ public class frmPrincipal extends JFrame implements ActionListener {
 	
 	private JPanel contentPane;
 	private JMenuItem mntmEmisionDePecosa;
-	public JLabel lblDatos;
 	private JMenuItem mntmCerrar;
+	private JMenuItem mntmInventarioInicial;
+	public JLabel lblDatos;
+	public static JLabel lblHora;
+	public static JLabel lblFecha2;
 	
 
 	/**
@@ -55,9 +67,12 @@ public class frmPrincipal extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public frmPrincipal() {
+		setForeground(SystemColor.textInactiveText);
+		setBackground(Color.WHITE);
+		setExtendedState(Frame.MAXIMIZED_HORIZ);
 		setTitle("LOGISTICA");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 823, 451);
+		setBounds(100, 100, 1000, 800);
 		this.setLocationRelativeTo(null);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -73,7 +88,8 @@ public class frmPrincipal extends JFrame implements ActionListener {
 		JMenu mnAlmacen = new JMenu("Almacen");
 		menuBar.add(mnAlmacen);
 		
-		JMenuItem mntmInventarioInicial = new JMenuItem("Inventario Inicial");
+		mntmInventarioInicial = new JMenuItem("Inventario Inicial");
+		mntmInventarioInicial.addActionListener(this);
 		mnAlmacen.add(mntmInventarioInicial);
 		
 		mntmEmisionDePecosa = new JMenuItem("Emision de PECOSA");
@@ -83,6 +99,7 @@ public class frmPrincipal extends JFrame implements ActionListener {
 		JMenu mnReporte = new JMenu("Reporte");
 		menuBar.add(mnReporte);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.activeCaption);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -90,11 +107,31 @@ public class frmPrincipal extends JFrame implements ActionListener {
 		lblDatos = new JLabel("Bienvenido:");
 		lblDatos.setForeground(Color.BLACK);
 		lblDatos.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDatos.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblDatos.setBounds(10, 11, 792, 23);
+		lblDatos.setFont(new Font("Tahoma", Font.BOLD, 23));
+		lblDatos.setBounds(319, 11, 545, 49);
 		contentPane.add(lblDatos);
+		
+		
+		lblHora = new JLabel("hora");
+		lblHora.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblHora.setBounds(166, 9, 120, 29);
+		contentPane.add(lblHora);
+		
+		lblFecha2 = new JLabel("fecha");
+		lblFecha2.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblFecha2.setBounds(10, 5, 158, 36);
+		contentPane.add(lblFecha2);
+		
+		MostraHora hora=new MostraHora();
+		hora.start();
+		
+			
+		
 	}
 	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == mntmInventarioInicial) {
+			actionPerformedMntmInventarioInicial(e);
+		}
 		if (e.getSource() == mntmCerrar) {
 			actionPerformedMntmCerrar(e);
 		}
@@ -109,5 +146,10 @@ public class frmPrincipal extends JFrame implements ActionListener {
 	protected void actionPerformedMntmCerrar(ActionEvent e) {
 		System.exit(0);
 	}
+	
+	protected void actionPerformedMntmInventarioInicial(ActionEvent e) {
+		
+	}
+	
 	
 }
